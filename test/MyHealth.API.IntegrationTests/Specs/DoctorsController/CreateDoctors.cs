ffb06a098323f116006f0f36c66fc035a15a00ed
tfsadmin <tfsadmin@TFS2017>
2017-03-28 07:44:24 +0000

    removing droid tests

--- test/MyHealth.API.IntegrationTests/Specs/DoctorsController/CreateDoctors.cs
+++ test/MyHealth.API.IntegrationTests/Specs/DoctorsController/CreateDoctors.cs
@@ -14,45 +14,45 @@ namespace MyHealth.API.Specs.DoctorsController
             Address = ""
         };
 
-        //[Fact]
-        //public async Task Administrator_Can_Create_Doctors()
-        //{
-        //    // Act
-        //    var response = await Server.CreateRequest(Api.Post.Doctors)
-        //        .WithIdentity(Identities.Administrator)
-        //        .WithDefaultPostHeaders()
-        //        .WithContent(_validDoctor)
-        //        .PostAsync();
+        [Fact]
+        public async Task Administrator_Can_Create_Doctors()
+        {
+            // Act
+            var response = await Server.CreateRequest(Api.Post.Doctors)
+                .WithIdentity(Identities.Administrator)
+                .WithDefaultPostHeaders()
+                .WithContent(_validDoctor)
+                .PostAsync();
 
-        //    // Assert
-        //    response.EnsureSuccessStatusCode();
-        //}
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
 
-        //[Fact]
-        //public async Task Tenant_Can_Create_Doctors()
-        //{
-        //    // Act
-        //    var response = await Server.CreateRequest(Api.Post.Doctors)
-        //        .WithIdentity(Identities.Tenant)
-        //        .WithDefaultPostHeaders()
-        //        .WithContent(_validDoctor)
-        //        .PostAsync();
+        [Fact]
+        public async Task Tenant_Can_Create_Doctors()
+        {
+            // Act
+            var response = await Server.CreateRequest(Api.Post.Doctors)
+                .WithIdentity(Identities.Tenant)
+                .WithDefaultPostHeaders()
+                .WithContent(_validDoctor)
+                .PostAsync();
 
-        //    // Assert
-        //    response.EnsureSuccessStatusCode();
-        //}
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
 
-        //[Fact]
-        //public async Task Users_Can_Create_Doctors()
-        //{
-        //    // Act
-        //    var response = await Server.CreateRequest(Api.Post.Doctors)
-        //        .WithDefaultPostHeaders()
-        //        .WithContent(_validDoctor)
-        //        .PostAsync();
+        [Fact]
+        public async Task Users_Can_Create_Doctors()
+        {
+            // Act
+            var response = await Server.CreateRequest(Api.Post.Doctors)
+                .WithDefaultPostHeaders()
+                .WithContent(_validDoctor)
+                .PostAsync();
 
-        //    // Assert
-        //    response.EnsureSuccessStatusCode();
-        //}
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
