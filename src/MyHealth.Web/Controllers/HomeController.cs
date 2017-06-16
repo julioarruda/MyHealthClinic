f8abf534bb6dcee81bd61299fc1b0af98b9c27b2
Julio Arruda <julio.arruda@outlook.com>
2017-06-16 13:43:54 -0300

    Alteração de Versão

--- src/MyHealth.Web/Controllers/HomeController.cs
+++ src/MyHealth.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace MyHealth.Web.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Version = "1.0.1";
+            ViewBag.Version = "2.0.0";
             return View();
         }
 
