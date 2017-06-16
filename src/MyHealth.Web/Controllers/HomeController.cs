c5de563ec628900447113a7eb987cc40a1dcf1c3
Julio Arruda <julio.arruda@outlook.com>
2017-06-16 14:17:03 -0300

    Correção do Bug

--- src/MyHealth.Web/Controllers/HomeController.cs
+++ src/MyHealth.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace MyHealth.Web.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Version = "1.0.1";
+            ViewBag.Version = "1.0.2";
             return View();
         }
 
